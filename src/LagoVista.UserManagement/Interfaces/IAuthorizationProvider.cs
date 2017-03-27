53248b25833a24ad92c155adf52cd09e5217edcb
Kevin D Wolf <kevinw@software-logistics.com>
2017-03-27 08:49:17 -0400

    add auth provider stubs

---
+++ src/LagoVista.UserManagement/Interfaces/IAuthorizationProvider.cs
@@ -0,0 +1,17 @@
+﻿using LagoVista.Core.Interfaces;
+using LagoVista.Core.Models;
+using LagoVista.UserManagement.Security;
+
+namespace LagoVista.UserManagement.Interfaces
+{
+    /* 
+     * This is responsible for two things
+     * 1) Ensure the user has access to the entity that they want to edit.
+     * 2) They have permissions to perform the behavior.
+     * 
+     */
+    public interface IAuthorizationProvider
+    {
+        AuthorizationResult AuthorizeAsync(IOwnedEntity entity, EntityHeader org, EntityHeader user);
+    }
+}
---
+++ src/LagoVista.UserManagement/Security/AuthorizationProvider.cs
@@ -0,0 +1,16 @@
+﻿using LagoVista.Core.Interfaces;
+using LagoVista.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.UserManagement.Security
+{
+    public class AuthorizationProvider
+    {
+        public AuthorizationResult AuthorizeAsync(IOwnedEntity entity, EntityHeader org, EntityHeader user)
+        {
+            return AuthorizationResult.Authorized;
+        }
+    }
+}
